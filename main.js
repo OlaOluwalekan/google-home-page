8a86d3716ac31ef7141dc1502417013fa8b7d8c0
OlaOluwalekan <olalekanbello534@gmail.com>
2022-10-08 15:03:17 +0100

    main content completed

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const searchInput = document.querySelector("#search");
 const searcBox = document.querySelector(".search-box");
 const clearIcon = document.querySelector(".clear-icon");
 
+const history = ["hello", "please", "quantum"];
+
 searchInput.onfocus = () => {
   searcBox.style.boxShadow = "0px 3px 5px #cfcfcf";
 };
@@ -16,10 +18,29 @@ searchInput.onkeyup = () => {
   } else {
     clearIcon.style.display = "flex";
   }
+  SearchQuery();
 };
+
+// searchInput.onmouseover = () => {
+//   searcBox.style.boxShadow = "0px 3px 5px #cfcfcf";
+// };
+
+// searchInput.onmouseout = () => {
+//   searcBox.style.boxShadow = "unset";
+// };
 
 clearIcon.onclick = () => {
   searchInput.value = "";
   searchInput.focus();
   clearIcon.style.display = "none";
+};
+
+const SearchQuery = () => {
+  // console.log(searchInput.value);
+
+  for (let i = 0; i < history.length; i++) {
+    if (history[i].includes(searchInput.value)) {
+      console.log(history[i]);
+    }
+  }
 };
